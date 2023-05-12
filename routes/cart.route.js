94a7b6f22b795dec52a67c63c9e1bfba97ffb310
animesh-0041 <ghoroianimesh1508@gmail.com>
2023-05-12 13:39:06 +0530

    update2

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const { cartModel } = require("../models/cart.model");
-const {userAuth}=require("../middlewares/users.middlewares")
-const {ProductModel}=require("../models/product.model");
+const { userAuth } = require("../middlewares/users.middlewares");
+const { ProductModel } = require("../models/product.model");
 const cartRouter = express.Router();
 
-cartRouter.get("/dummy",async(req,res)=>{
+cartRouter.get("/dummy", async (req, res) => {
   try {
-    const data=await cartModel.find();
-    res.send(data)
+    const data = await cartModel.find();
+    res.send(data);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-})
+});
 
-cartRouter.use(userAuth)
+cartRouter.use(userAuth);
 //getting all the products
 cartRouter.get("/", async (req, res) => {
   const { userID } = req.body;
@@ -21,18 +21,18 @@ cartRouter.get("/", async (req, res) => {
     const posts = await cartModel.find({ userID });
     res.status(200).send(posts);
   } catch (error) {
-    res.status(400).send({ "msg": error.message });
+    res.status(400).send({ msg: error.message });
   }
 });
 
 //add new product to cart
 cartRouter.post("/add", async (req, res) => {
-  req.body.quantity=1
+  req.body.quantity = 1;
   const payload = req.body;
   try {
     const post = new cartModel(payload);
     await post.save();
-    res.status(200).send({ "msg": "A new product has been added to cart" });
+    res.status(200).send({ msg: "A new product has been added to cart" });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -41,18 +41,14 @@ cartRouter.post("/add", async (req, res) => {
 //update a data in the cart page
 cartRouter.patch("/update/:id", async (req, res) => {
   let { id } = req.params;
-  const cartItem=await cartModel.findOne({_id:id})
+  const cartItem = await cartModel.findOne({ _id: id });
   try {
-
-      if(req.body.userID==cartItem.userID){
-
-        await cartModel.findByIdAndUpdate({ _id: id },req.body);
-        res.status(200).send({ "msg": `product ${id} has been updated` });
-      }else{
-        res.status(400). send({"msg":"You are not authenticate!"})
-      }
-
-
+    if (req.body.userID == cartItem.userID) {
+      await cartModel.findByIdAndUpdate({ _id: id }, req.body);
+      res.status(200).send({ msg: `product ${id} has been updated` });
+    } else {
+      res.status(400).send({ msg: "You are not authenticate!" });
+    }
   } catch (error) {
     res.status(404).send(error);
   }
@@ -61,37 +57,31 @@ cartRouter.patch("/update/:id", async (req, res) => {
 //delete a data from the cart
 cartRouter.delete("/delete/:id", async (req, res) => {
   let { id } = req.params;
-  const cartItem=await cartModel.findOne({_id:id})
+  const cartItem = await cartModel.findOne({ _id: id });
   try {
-
-      if(req.body.userID==cartItem.userID){
-
-        await cartModel.findByIdAndDelete({ _id: id });
-        res.status(200).send({ "msg": `product ${id} has been deleted` });
-      }else{
-        res.status(400). send({"msg":"You are not authenticate!"})
-      }
-
-
+    if (req.body.userID == cartItem.userID) {
+      await cartModel.findByIdAndDelete({ _id: id });
+      res.status(200).send({ msg: `product ${id} has been deleted` });
+    } else {
+      res.status(400).send({ msg: "You are not authenticate!" });
+    }
   } catch (error) {
     res.status(404).send(error);
   }
 });
 
 //delete all cart product current user
-cartRouter.delete("/delete", async (req, res) => { 
+cartRouter.delete("/delete", async (req, res) => {
+  const {userID}=req.body
+  console.log(userID);
   try {
-        await cartModel.deleteMany({ userID: req.body.userID });
-        res.status(200).send({ "msg": `product ${id} has been deleted` });
-    
-
-
+    await cartModel.deleteMany({userID });
+    res.status(200).send({ msg: `product has been deleted` });
   } catch (error) {
     res.status(404).send(error);
   }
 });
 
-
 module.exports = {
-  cartRouter
+  cartRouter,
 };
