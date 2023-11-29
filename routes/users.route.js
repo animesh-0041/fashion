b166fc6ae8aad385cb146462d0c50627c4d201b1
animesh-0041 <ghoroianimesh1508@gmail.com>
2023-11-29 18:45:05 +0530

    update response of login

--- routes/users.route.js
+++ routes/users.route.js
@@ -44,7 +44,7 @@ userRoute.post("/login",async(req,res)=>{
                         res.send({"msg":"login successful","token":token,"email":user.email,"name":user.firstName+" "+user.lastName})
                       
                    }else{
-                    res.status(400).send({"msg":"Incorrect password!"})
+                    res.send({"msg":"Incorrect password!"})
                    }
                 });
                 
@@ -53,7 +53,7 @@ userRoute.post("/login",async(req,res)=>{
             }
 
         }else{
-            res.status(400).send({"msg":"User not found!"})
+            res.send({"msg":"User not found!"})
         }
     
 })
